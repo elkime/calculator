b3b23be64b2d72dd3de632c20b62de3aa1e5ff56
elkime <elkmejia@gmail.com>
2024-05-06 21:17:29 -0500

    first commit calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,9 @@
+const buttonNumbers = document.getElementsByName("data-number");
+const buttonOpera = document.getElementsByName("data-opera");
+const buttonEqual = document.getElementsByName("data-equal");
+const buttonDelete = document.getElementsByName("data-delete");
+let result = document.getElementById('result');
+
+//console.log(buttonEqual);
+
+console.log(result);
